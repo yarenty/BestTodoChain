f3fceb5e0a9f818020b8309f7fe06ff2f83c534f
yarenty <yarenty@gmail.com>
2015-03-27 17:37:07 +0000

    play around

--- index.ios.js
+++ index.ios.js
@@ -1,28 +1,38 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- */
-'use strict';
-
 var React = require('react-native');
+
+var MOCKED_MOVIES_DATA = [
+  {title: 'Title', year: '2015', posters: {thumbnail: 'http://i.imgur.com/UePbdph.jpg'}},
+];
+
 var {
   AppRegistry,
+  Image,
   StyleSheet,
   Text,
   View,
 } = React;
 
+
 var BestTodoChain = React.createClass({
   render: function() {
+  	var movie = MOCKED_MOVIES_DATA[0];
+
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>
           Welcome to React Native!
         </Text>
+        <Text style={styles.cool}>
+        JARO RuleZ
+        </Text>
         <Text style={styles.instructions}>
           To get started, edit index.ios.js{'\n'}
           Press Cmd+R to reload
         </Text>
+        <Text>{movie.title}</Text>
+        <Text>{movie.year}</Text>
+        <Image source={{uri: movie.posters.thumbnail}} style={styles.thumbnail}/>
+
       </View>
     );
   }
@@ -40,10 +50,21 @@ var styles = StyleSheet.create({
     textAlign: 'center',
     margin: 10,
   },
+  cool: {
+    fontSize: 18,
+    textAlign: 'center',
+    margin: 10,
+    color: '#FF3333'
+  },
   instructions: {
     textAlign: 'center',
     color: '#333333',
   },
+  thumbnail: {
+    width: 53,
+    height: 81,
+  },
 });
+
 
 AppRegistry.registerComponent('BestTodoChain', () => BestTodoChain);
